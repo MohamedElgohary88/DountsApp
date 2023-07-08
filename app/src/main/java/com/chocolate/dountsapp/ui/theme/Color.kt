ac1af5b450104f1f126467c49d6f2fdb75d99487
Mohamed Elgohary <bondokelgohary@gmail.com>
2023-07-08 23:40:15 +0300

    add colors

--- app/src/main/java/com/chocolate/dountsapp/ui/theme/Color.kt
+++ app/src/main/java/com/chocolate/dountsapp/ui/theme/Color.kt
@@ -9,3 +9,8 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val Pink87 = Color(0xFFFF7074)
+val Pink67 = Color(0xFFFF9494)
+val Pink38 = Color(0xFFFED8DF)
+val BlueSky = Color(0xFFD7E4F6)
