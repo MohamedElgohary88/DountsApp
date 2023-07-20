0e21aa89c057ed34aaed6e3257f6a2b7338fe52a
Mohamed Elgohary <bondokelgohary@gmail.com>
2023-07-20 11:13:18 +0300

    cleanup

--- app/src/main/java/com/chocolate/dountsapp/screens/home/composeable/DountItemTop.kt
+++ app/src/main/java/com/chocolate/dountsapp/screens/home/composeable/DountItemTop.kt
@@ -3,6 +3,8 @@ package com.chocolate.dountsapp.screens.home.composeable
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsPressedAsState
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -18,6 +20,10 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -48,10 +54,14 @@ fun DonutItemTop(
     onClick: () -> Unit,
     modifier: Modifier = Modifier
 ) {
+    var isPressed by remember { mutableStateOf(false) }
     Box(
         contentAlignment = Alignment.CenterEnd,
         modifier = modifier
-            .clickable { onClick() }
+            .clickable {
+                isPressed = false
+                onClick()
+            }
             .height(320.dp)
             .width(193.dp)
     ) {
